d30b0617ee5e6656ca657d006b6096b92d25cb3d
Pedro Malaquias <psmalaquias1999@gmail.com>
2023-01-26 00:10:49 -0300

    Inserting the generated images in the repository

--- Spiral/spiral.py
+++ Spiral/spiral.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
     nnfs.init()
 
     # Create dataset
-    points, targetClasses = spiral_data(samples = 1000, classes=3)
+    points, targetClasses = spiral_data(samples=1000, classes=3)
     # classes[i] means that points[i] belongs to the class classes[i].
     data = list(zip(points, targetClasses))
     random.shuffle(data)
     trainingData = data[:1000]
     testData = data[1000:1150]
-    #plt.scatter(points[:, 0], points[:, 1])
-    #plt.scatter(points[:, 0], points[:, 1], c=targetClasses, cmap='brg')
-    #plt.show()
+    # plt.scatter(points[:, 0], points[:, 1])
+    # plt.scatter(points[:, 0], points[:, 1], c=targetClasses, cmap='brg')
+    # plt.show()
 
     neuralNetwork = NeuralNetwork([2, 64, 64, 3])
     neuralNetwork.train(trainingData, testData)
